e0ae6bb4b138ee10a11b01eaf22d6e73db48b402
Junrill Galvez <junrillg@gmail.com>
2018-10-16 04:38:34 +0800

    format code

--- backend/app.js
+++ backend/app.js
@@ -6,21 +6,25 @@ const postsRoutes = require('./routes/posts');
 
 const app = express();
 
-mongoose.connect(process.env.DB_MONGO_URI)
-.then(() => console.log('Connected to Database!'))
-.catch(() => console.log('Connection Failed!'))
+mongoose
+    .connect(process.env.DB_MONGO_URI)
+    .then(() => console.log('Connected to Database!'))
+    .catch(() => console.log('Connection Failed!'));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS, PUT');
-  next();
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PATCH, DELETE, OPTIONS, PUT'
+    );
+    next();
 });
 
 app.use('/api/posts', postsRoutes);
